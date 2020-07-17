e1f57b37ce8add5b2ad29fa1eeec1ce59e0c3e98
Anna <Anna@MacBook-Air-Andrey.local>
2020-07-17 16:47:21 +0200

    add check for login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,11 +1,14 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -13,4 +16,4 @@ class MainPage(BasePage):
             return False
         return True    
     def should_be_login_link(self):  
-    	assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
+    	assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
