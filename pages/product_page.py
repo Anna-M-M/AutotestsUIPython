1c17b1489ca801a41e0e07699f63e1708d1a4904
Anna <Anna@MacBook-Air-Andrey.local>
2020-07-28 14:23:59 +0200

    add test_product_page and product_page, change locators and base_page for testing product can be added to basket

---
+++ pages/product_page.py
@@ -0,0 +1,30 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+from selenium.webdriver.common.by import By
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        basket_button = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket_button.click()
+        self.solve_quiz_and_get_code()
+
+    def should_item_in_basket(self):
+        self.should_cost_equal()
+        self.should_name_equal()
+
+    def should_cost_equal(self):
+        item_basket_cost = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL)
+        item_product_cost = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        assert item_basket_cost.text == item_product_cost.text, "Prices in basket and in product page isn't equal"
+
+    def should_name_equal(self):
+        items_strong = self.browser.find_elements(*ProductPageLocators.BASKET_STRONG_NAMES)
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        basket_product_name = ''
+        names_equal = False
+        for item_strong in items_strong:
+            if item_strong.text == product_name:
+                names_equal = True
+        assert names_equal, "Names of product isn't equal"    
+
+       
