c08a7185d415255a48018ac25e6cf7a1dfc50936
Anna <Anna@MacBook-Air-Andrey.local>
2020-08-17 11:42:06 +0200

    Add BasePageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,15 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
 	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")    
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")	
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
+    BASKET_STRONG_NAMES = (By.CSS_SELECTOR, ".alertinner strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
