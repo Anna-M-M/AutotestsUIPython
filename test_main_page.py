1038a2f484c488d576cc84da3197f73fc770f5ac
Anna <Anna@MacBook-Air-Andrey.local>
2020-08-19 15:07:50 +0200

    Add test_guest_cant_see_product_in_basket_opened_from_main_page and test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,7 @@
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -20,3 +16,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()    
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.should_not_be_item_in_basket()
+    page.should_be_empty_basket()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):   
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.should_not_be_item_in_basket()
+    page.should_be_empty_basket()
