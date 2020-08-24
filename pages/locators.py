222b3ea92303d6722e1ba73396f6a338f014fc40
Anna <Anna@MacBook-Air-Andrey.local>
2020-08-24 12:34:26 +0200

    Add new locators USER_ICON, REGISTER_USERNAME, REGISTER_PASSWORD1, REGISTER_PASSWORD2 and REGISTER_BUTTON

--- pages/locators.py
+++ pages/locators.py
@@ -5,8 +5,12 @@ class MainPageLocators():
     BASKET_LINK = (By.CSS_SELECTOR,".hidden-xs > span > a")
 
 class LoginPageLocators():
-	LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
-	REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")    
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")  
+    REGISTER_USERNAME =  (By.CSS_SELECTOR,"#id_registration-email")
+    REGISTER_PASSWORD1 = (By.CSS_SELECTOR,"#id_registration-password1")
+    REGISTER_PASSWORD2 = (By.CSS_SELECTOR,"#id_registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")	
@@ -19,6 +23,7 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():    
     ITEM_IN_BASKET = (By.CSS_SELECTOR, ".basket-items")
